ebe4c98dbd6b2cd1a446e5ad261e71fc7fc8b83b
GuzzyWasTaken <guzzy@Zaza.localdomain>
2023-07-10 14:47:45 +0200

    Harl completed

--- cpp01/ex05/Harl.hpp
+++ cpp01/ex05/Harl.hpp
@@ -6,13 +6,16 @@
 # include <iomanip>
 # include <fstream>
 
-typedef enum e_state
-{
-    DEBUG,
-    INFO,
-    WARNING,
-    ERROR,
+struct State {
+    std::string code;
+    std::string message;
 };
+
+State DEBUG = {"DEBUG", "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!"};
+State INFO = {"INFO", "I cannot believe adding extra bacon costs more money. You didnt put enough bacon in my burger! if you did, I wouldn't be asking for it!"};
+State WARNING = {"WARNING", "I think I desertve to have some extra bacon for free. I've been coming for years you started working since last month"};
+State ERROR = {"ERROR", "This is unnacceptable, I want to speak to the manager now!"};
+
 
 typedef class Harl
 {
@@ -25,7 +28,8 @@ typedef class Harl
     public:
         ~Harl();
         Harl();
-        voi complain(std::string level);
+        void complain(std::string level);
 };
 
+typedef void (Harl::*t_func) (void);
 #endif
--- cpp01/ex05/main.cpp
+++ cpp01/ex05/main.cpp
@@ -12,10 +12,61 @@ int main(int ac, char **av)
 {
     Harl guy;
 
-    guy.complain
+
+
 }
 
 void Harl::complain(std::string level)
 {
+    int state = 0;
+    t_func funcs[] = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+    std::string levels[] = { "DEBUG", "INFO", "WARNING", "ERROR"};
+    while(level.compare(levels[state]) != 0)
+    {
+        state++;
+        if (state > 4)
+        {
+            std::cout << "something went wrong" << std::endl;
+            return;
+        }
+    }
+
+    switch (state)
+    {
+        case 0:
+                this->*funcs[state]();
+        case 1:
+                this->*funcs[state]();
+        case 2:
+                this->*funcs[state]();
+        case 3:
+                this->*funcs[state]();
+                break;
+    }
     
+
+}
+
+void Harl::debug(void)
+{
+    State currentState = DEBUG;
+    std::cout << currentState.message << std::endl;
+}
+
+void Harl::info(void)
+{
+    State currentState = INFO;
+    std::cout << currentState.message << std::endl;
+}
+
+void Harl::warning(void)
+{
+    State currentState = WARNING;
+    std::cout << currentState.message << std::endl;
+}
+
+void Harl::error(void)
+{
+    State currentState = DEBUG;
+    std::cout << currentState.message << std::endl;
 }
