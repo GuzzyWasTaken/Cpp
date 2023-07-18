ec5d671572adc4c72354c6fa95c9bf9ef6be5745
GuzzyWasTaken <guzzy@Zaza.localdomain>
2023-07-18 18:33:08 +0200

    undone

---
+++ cpp02/ex01/Fixed.cpp
@@ -0,0 +1,46 @@
+#include "Fixed.hpp"
+
+//Roundf just rounds a nmber
+//
+
+Fixed::Fixed(void)
+{
+    std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const int num)
+{
+
+    std::cout << "constructer made" << std::endl;
+    this->value = 0;
+}
+Fixed::~Fixed()
+{
+    std::cout << "Deconstructing" << std::endl;
+}
+
+Fixed::Fixed(const Fixed& other)
+{
+    std::cout << "copy constructor made" << std::endl;
+    if (this->value != other.value)
+        this->value = other.value;
+}
+
+Fixed& Fixed::operator=(const Fixed& other)
+{
+    std::cout << "operator overloaded" << std::endl;
+    this->value = other.value;
+    return *this;
+}
+
+int Fixed::getRawBits(void) const
+{
+    std::cout << "returned raw bits:" << this->value << std::endl;
+    return(this->value);
+}
+
+void Fixed::setRawBits(int const raw)
+{
+    std::cout << "set raw bits to:" << raw << std::endl;
+    this->value = raw;
+}
